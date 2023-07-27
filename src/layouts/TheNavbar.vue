76ac138795cf4a429ef1dcc5d516182b371152d1
Leonardo Nicola <leonardonicolares@gmail.com>
2023-07-27 02:10:50 -0300

    fix: change component name

--- src/layouts/TheNavbar.vue
+++ src/layouts/TheNavbar.vue
@@ -10,47 +10,36 @@ const { currentUser } = storeToRefs(store)
 
 function logout() {
   store.clearCurrentUser()
-  window.localStorage.removeItem('token')
+  localStorage.removeItem('token')
   router.push({ name: 'home' })
 }
 </script>
 
 <template>
-  <div class="navbar bg-base-300">
+  <div class="navbar bg-base-100">
     <div class="navbar-start">
-      <div class="dropdown">
+      <RouterLink to="/" class="btn btn-ghost font-black normal-case text-xl">Drinkify</RouterLink>
+    </div>
+
+    <div class="navbar-end">
+      <div class="form-control">
+        <input type="text" placeholder="Busque por drinks..." class="input input-bordered w-full" />
+      </div>
+      <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
+          <UserIcon :size="26" />
         </label>
         <ul
           tabindex="0"
           class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52 font-extrabold"
         >
+          <li v-if="currentUser">
+            <RouterLink to="/profile">{{ currentUser.name }}</RouterLink>
+          </li>
           <li v-if="!currentUser"><RouterLink to="/login">Login</RouterLink></li>
           <li v-if="currentUser" @click="logout"><a>Sair</a></li>
         </ul>
       </div>
-    </div>
-    <div class="navbar-center">
-      <RouterLink to="/" class="btn btn-ghost normal-case text-xl">Drinkify</RouterLink>
-    </div>
-    <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle" v-if="currentUser">
-        <RouterLink to="/profile"><UserIcon :size="26" /></RouterLink>
-      </button>
     </div>
   </div>
 </template>
--- src/views/Login.cy.ts
+++
@@ -1,8 +0,0 @@
-import Login from './Login.vue'
-
-describe('<Login />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(Login)
-  })
-})
